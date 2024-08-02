082a0a1f86a0a2991fa65bee607e12056ffcb30e
Alexey Larionov <alxs.larionov@gmail.com>
2024-08-02 19:24:35 +0300

    Resize callback

--- src/app/Main.cpp
+++ src/app/Main.cpp
@@ -1,33 +1,58 @@
-#include <iostream>
 #include <engine/GlHelpers.hpp>
+
 #include <spdlog/spdlog.h>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+#include <vector>
+
+struct WindowCtx {
+   engine::isize width;
+   engine::isize height;
+
+   WindowCtx(GLFWwindow* window) {
+      int w, h;
+      glfwGetFramebufferSize(window, &w, &h);
+      width = w;
+      height = h;
+   }
+};
+
+struct RenderCtx final {
+   engine::i64 timeNs{};
+   engine::f32 timeSec{};
+   engine::i64 prevTimeNs{};
+   engine::f32 prevFrametimeMs{};
+   engine::f32 prevFPS{};
+
+   void Update(engine::i64 currentTimeNs, RenderCtx& destination) const {
+      destination.timeNs = currentTimeNs;
+      destination.timeSec = static_cast<engine::f32>(currentTimeNs / 1000) * 0.000001;
+      destination.prevTimeNs = this->timeNs;
+      engine::f32 frametimeMs = static_cast<engine::f32>(currentTimeNs - this->timeNs) * 0.000001;
+      destination.prevFrametimeMs = frametimeMs;
+      destination.prevFPS = 1000.0 / frametimeMs;
+   }
+};
 
 static void GlfwErrorCallback(int errCode, char const* message) {
    XLOG("GLFW_ERROR({}): {}", errCode, message);
+}
+
+static void GlfwResizeCallback(GLFWwindow* window, int width, int height) {
+   glViewport(0, 0, width, height);
+   auto ctx = static_cast<WindowCtx*>(glfwGetWindowUserPointer(window));
+   ctx->width = width;
+   ctx->height = height;
 }
 
 static void Terminate() {
    glfwTerminate();
 }
 
-struct RenderCtx {
-   engine::isize width{};
-   engine::isize height{};
-   engine::i64 timeNs{};
-   engine::f32 timeSec{};
-
-   void Update() {
-      timeSec = static_cast<engine::f32>(timeNs / 1000) * 0.000001;
-   }
-};
-
 static void Render(RenderCtx const& ctx) {
-   glViewport(0, 0, ctx.width, ctx.height);
-   engine::f32 red = 0.5f * (std::sin(ctx.timeSec) + 1.0f);
-   XLOG("{} {}", red, ctx.timeSec);
+   engine::f32 const red = 0.5f * (std::sin(ctx.timeSec) + 1.0f);
    glClearColor(red, 0.5f, 0.5f, 0.0f);
    glClear(GL_COLOR_BUFFER_BIT);
 }
@@ -49,15 +74,16 @@ int main() {
    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-   engine::isize winWidth = 800, winHeight = 600;
-   GLFWwindow* window = glfwCreateWindow(winWidth, winHeight, "LearnOpenGL", NULL, NULL);
+   GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
    if (window == nullptr) {
       XLOG("Failed to create GLFW window", 0)
       Terminate();
       return -1;
    }
    glfwMakeContextCurrent(window);
+   glfwSetFramebufferSizeCallback(window, GlfwResizeCallback);
    glfwSwapInterval(1);
+   glfwSetTime(0.0);
 
    if (!gladLoadGL(static_cast<GLADloadfunc>(glfwGetProcAddress))){
       XLOG("Failed to initialize GLAD", 0);
@@ -65,23 +91,29 @@ int main() {
       return -1;
    }
 
-   RenderCtx renderCtx{};
-   {
-      int width, height;
-      glfwGetFramebufferSize(window, &width, &height);
-      renderCtx.width = width;
-      renderCtx.height = height;
-   }
+   WindowCtx windowCtx(window);
+   glfwSetWindowUserPointer(window, &windowCtx);
+   std::vector<RenderCtx> frameHistory(256);
+   size_t frameIdx = 0, prevFrameHistoryIdx = 0;
 
-   renderCtx.timeNs = 0;
    while (!glfwWindowShouldClose(window))
    {
-      renderCtx.timeNs = glfwGetTimerValue();
-      renderCtx.Update();
+      size_t const frameHistoryIdx = (prevFrameHistoryIdx + 1) % frameHistory.size();
+      RenderCtx& renderCtx = frameHistory[frameHistoryIdx];
+      RenderCtx const& prevRenderCtx = frameHistory[prevFrameHistoryIdx];
+      prevRenderCtx.Update(glfwGetTimerValue(), renderCtx);
+
       Render(renderCtx);
+
+      if (frameHistoryIdx == 0) {
+         XLOG("{} FPS, {} ms", renderCtx.prevFPS, renderCtx.prevFrametimeMs);
+      }
 
       glfwSwapBuffers(window);
       glfwPollEvents();
+
+      prevFrameHistoryIdx = frameHistoryIdx;
+      ++frameIdx;
    }
 
    Terminate();
